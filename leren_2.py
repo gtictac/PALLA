e8354b99540aa77657ca53338250be199a34b26a
eggwise <dev@eggwise.com>
2016-11-12 00:45:42 +0100

    nieuwe les

---
+++ leren_2.py
@@ -0,0 +1,28 @@
+persoon = 'Glen'
+
+print('Wilt u koffie?')
+antwoord = input()
+print('Uw antwoord was:')
+print(antwoord)
+
+if antwoord == 'ja':
+    keuze = True
+else:
+    keuze = False
+
+will_je_koffie = None
+
+if keuze is True:
+    print('Ja ik wil koffie')
+else:
+    print('ik wil geen koffie')
+
+
+
+
+
+
+
+
+
+
