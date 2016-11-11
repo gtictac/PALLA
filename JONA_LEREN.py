7974a14dcd347d6658ea6d88c69fbe394290929a
Bender <glentiktak@gmail.com>
2016-11-12 01:27:05 +0200

    Ja
    Nee

---
+++ JONA_LEREN.py
@@ -0,0 +1,62 @@
+#dit is een python file
+# met deze teken :  >>>> # <<< geef ik commentaar aan
+
+
+#dit is commentaar
+
+#in de programmeerwereld verwerk je informatie, je een simpele sommen oplossen en complete functionaliteiten maken met code
+
+#simpele som
+
+3 + 2
+
+#dit doet verder niets, als je in pycharm kijkt staat er ook , "statement seems to have no effect" als je je muis erop houdt
+
+#een statement is een UITDRUKKING
+
+#stel we willen graag iets met 3 + 2 doen.
+#de uitkomst krijgen en weergeven bijvoobeeld.
+#om de uitkomst te krijgen, moeten we het resultaat van de som ergens opslaan.
+#dit ergens, is een VARIABELE
+
+#variabele: iets dat een waarde heeft.
+#in het geval van deze simpele som is een variabele bijvoorbeeld de uitkomst ervan.
+#zie hier
+uitkomst = 3 + 2
+
+# VARIABELE INFORMATIE
+
+#een variabele DECLAREER je als volgt: variabele_naam = EXPRESSIE
+#variabele in python doe je meestal in camel_case, variabele mogen niet beginnen met nummers geen punten bevatten en zo zijn er nog wat regels.
+#lees over variabelen, expressies, statements
+#google trouwens het verschil tussen een STATEMENT en een EXPRESSIE (voor betere uitleg)
+
+
+
+#OKE VERDER
+
+#we hebben nu de waarde van de statement 2 + 3 opgeslagen in de variabele "uitkomst"
+#als we deze willen weergeven dan printen we de waarde naar de console (command line)
+# dit kan met de functie print
+#zie hier
+print(uitkomst)
+
+
+# 3 + 2 is leuk en aardig, maar we kunnen er weinig mee.
+#stel je wil de uitkomst van 2 getallen weten, en deze weergeven.
+#als je een variabele maakt van de getallen die je bij elkaar op kan tellen dan kun je deze later makkelijk aanpassen
+#en HERGEBRUIKEN, hergebruiken  / dont reinvent the weel / Dont repeat yourself, (google: DRY code) is een van de belangrijkste
+#programmeer "pilaren"
+
+#oke, dus 2 getallen bij elkaar optellen
+#maak de variabelen
+
+getal_1 = 5
+getal_2 = 10
+
+#uitkomst:
+de_uitkomst = getal_1 + getal_2
+
+print(de_uitkomst)
+
+
